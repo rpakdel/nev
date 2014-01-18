ecb4e3f7da46f7d58f9d53d6882f608deb2d0e46
Reza Pakdel <rpakdel@gmail.com>
2014-01-18 11:14:09 -0800

    Added artefacts dir

--- config.js
+++ config.js
@@ -6,12 +6,15 @@ var httpServerPort = 8080;
 var publicDir = path.join(__dirname, 'public');
 var uploadDir = path.join(publicDir, 'eyefi');
 var imagesDir = path.join(publicDir, 'images');
+var artefactsDir = path.join(uploadDir, 'artefacts');
 var processOnlyOneImage = true;
 var showRunDemoButton = true;
 
 exports.publicDir = publicDir;
 exports.uploadDir = uploadDir;
 exports.imagesDir = imagesDir;
+exports.artefactsDir = artefactsDir;
+
 exports.httpServerPort = httpServerPort;
 exports.httpServerIP = httpServerIp;
 exports.httpServer = httpServerIp + ':' + httpServerPort;
